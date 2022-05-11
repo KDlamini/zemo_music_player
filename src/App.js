684ebe02459736f8815aa362232cea0cb91448bd
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-05-11 18:20:12 +0200

    Make songs array its own component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import Player from './components/Player'
+import music from './components/Music';
 
 function App() {
-    const [songs] = useState([
-        {
-            title: "Golden",
-            artist: "Harry Styles",
-            img_src: "./images/harry-syles-fine-line-album-cover.jpeg",
-            src: "./music/Harry Styles - Golden (Official).mp3"
-        },
-        {
-            title: "Watermerlon Sugar",
-            artist: "Harry Styles",
-            img_src: "./images/harry-syles-fine-line-album-cover.jpeg",
-            src: "./music/Harry Styles - Watermelon Sugar (Official Audio).mp3"
-        },
-        {
-            title: "Peaches ft. Daniel Caesar, Giveon",
-            artist: "Justin Bieber",
-            img_src: "./images/Justin_Bieber_-_Justice.png",
-            src: "./music/Justin Bieber - Peaches ft. Daniel Caesar, Giveon.mp3"
-        },
-        {
-            title: "Streets",
-            artist: "Doja Cat",
-            img_src: "./images/doja-cat-street-cover.jpeg",
-            src: "./music/Doja Cat - Streets (Audio).mp3"
-        },
-        {
-            title: "Montero (Call Me By Your Name",
-            artist: "Lil Nas X",
-            img_src: "./images/lil-nas-x-call-me-by-your-name.jpg",
-            src: "./music/Lil Nas X - MONTERO (Call Me By Your Name).mp3"
-        }
-    ]);
-
+    const [songs] = useState(music);
+    const songsLength = songs.length;
     const [currentSongIndex, setCurrentSongIndex] = useState(0);
     const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
     useEffect(() => {
         setNextSongIndex(() => {
-            if(currentSongIndex + 1 > songs.length - 1) {
+            if(currentSongIndex + 1 > songsLength - 1) {
                 return 0;
             } else {
                 return currentSongIndex + 1;
             }
         })
-    }, [currentSongIndex])
+    }, [currentSongIndex, songsLength]);
 
     return (
         <div className="App">
---
+++ src/components/Music.js
@@ -0,0 +1,34 @@
+const music = [
+    {
+        title: "Golden",
+        artist: "Harry Styles",
+        img_src: "./images/harry-syles-fine-line-album-cover.jpeg",
+        src: "./music/Harry Styles - Golden (Official).mp3"
+    },
+    {
+        title: "Watermerlon Sugar",
+        artist: "Harry Styles",
+        img_src: "./images/harry-syles-fine-line-album-cover.jpeg",
+        src: "./music/Harry Styles - Watermelon Sugar (Official Audio).mp3"
+    },
+    {
+        title: "Peaches ft. Daniel Caesar, Giveon",
+        artist: "Justin Bieber",
+        img_src: "./images/Justin_Bieber_-_Justice.png",
+        src: "./music/Justin Bieber - Peaches ft. Daniel Caesar, Giveon.mp3"
+    },
+    {
+        title: "Streets",
+        artist: "Doja Cat",
+        img_src: "./images/doja-cat-street-cover.jpeg",
+        src: "./music/Doja Cat - Streets (Audio).mp3"
+    },
+    {
+        title: "Montero (Call Me By Your Name",
+        artist: "Lil Nas X",
+        img_src: "./images/lil-nas-x-call-me-by-your-name.jpg",
+        src: "./music/Lil Nas X - MONTERO (Call Me By Your Name).mp3"
+    }
+]
+
+export default music;
